4a5b21158e721b22a44d82461ed7857c1a86973c
John Hinojosa <hinojohn@ymail.com>
2021-06-23 14:49:01 -0500

    added missing dependency to package, altered routing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
+import Detail from "./pages/Detail";
 import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Search />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/search"]}>
+            <Search />
+          </Route>
+          <Route exact path={"/saved"}>
+            <Saved />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
